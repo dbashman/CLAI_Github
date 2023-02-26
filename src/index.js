827d39f63492c58da503ea91efe4e052d3c497c1
dbashman <126377318+dbashman@users.noreply.github.com>
2023-02-26 12:18:42 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Cover Letter AI</h1>
+<div>
+  Copy paste your resume and Job Description
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
